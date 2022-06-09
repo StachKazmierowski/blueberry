3a379ca381a04545865524b460b7d55617d7ab03
StachKazmierowski <staho.kazmierowski@gmail.com>
2022-06-09 15:01:02 +0200

    main prepared for first tests

--- main.cpp
+++ main.cpp
@@ -7,10 +7,23 @@
 #include "src/algorithm/algorithm.h"
 using namespace std;
 
-int main(){
-//    int A = 15, B = 15, n = 10;
-//    vector<vector<int>> all_divs = all_divisions(A, n);
-//    cout << all_divs.size() << endl;
-    Game g = algorithm(20, 20, 15);
+int main(int argc, char **argv){
+    if (argc != 4) {
+        cout << "Usage:\n"
+                "Number of playerA's troops\n"
+                "Number of playerB's troops\n"
+                "Number of battlefields\n";
+        return 1;
+    }
+    auto A = (int) strtol(argv[1], nullptr, 10);
+    auto B = (int) strtol(argv[2], nullptr, 10);
+    auto n = (int) strtol(argv[3], nullptr, 10);
+
+    auto start = chrono::steady_clock::now();
+    Game g = algorithm(A, B, n);
+    auto end = chrono::steady_clock::now();
+    cout << "Elapsed time in milliseconds: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " ms" << endl;
     return 0;
 }
